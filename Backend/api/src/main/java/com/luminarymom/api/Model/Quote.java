f24fdae79763e8d98711632018960d32f0048013
Roxanne Athena Reindl <coldbrewapps@yahoo.com>
2026-06-18 14:00:28 -0700

    Quote data model file created

--- Backend/api/src/main/java/com/luminarymom/api/Model/Quote.java
+++ Backend/api/src/main/java/com/luminarymom/api/Model/Quote.java
@@ -1,4 +1,28 @@
 package com.luminarymom.api.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "quotes")
 public class Quote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String text;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String category;
+
 }
